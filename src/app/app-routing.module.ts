d6a54d9e102abebf82ba99a16f5a5496331630bb
Carrizane <miguelcarrizo@upeu.edu.pe>
2019-10-25 11:28:54 -0500

    componente login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PlantillaComponent } from './ComponentesVista/plantilla/plantilla.component';
+import { LoginComponent } from './ComponentesVista/Seguridad/login/login.component';
 
 const routes: Routes = [
   {path:'', component:PlantillaComponent},
-  {path:'ctc', component:PlantillaComponent}
+  {path:'ctc', component:PlantillaComponent},
+  {path:'login', component:LoginComponent}
 ];
 
 @NgModule({
@@ -14,5 +16,6 @@ const routes: Routes = [
 export class AppRoutingModule { }
 
 export const routingComponents = [
-  PlantillaComponent
+  PlantillaComponent,
+  LoginComponent
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './ComponentesGlobal/sidebar/sidebar.component';
-import { LoginComponent } from './ComponentesVista/Seguridad/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    routingComponents,
-    LoginComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
